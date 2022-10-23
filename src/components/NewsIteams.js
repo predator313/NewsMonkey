ea4de449cc6c4c0634b5b761f5fdc9821f97fe63
Aamir Ashraf <aamirnasa97@gmail.com>
2022-10-24 02:18:06 +0530

    adding news api

---
+++ src/components/NewsIteams.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export class NewsIteams extends Component {
+ 
+  render() {
+    let { title, description,imgurl,newsurl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card " style={{ width: "18rem" }}>
+          <img src={imgurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">
+              For more news
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsIteams;
