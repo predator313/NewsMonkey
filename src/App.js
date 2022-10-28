fced5291c03ddf696bd92d4ede0533297d8d84eb
Aamir Ashraf <aamirnasa97@gmail.com>
2022-10-28 23:08:28 +0530

    hiding api key

--- src/App.js
+++ src/App.js
@@ -4,39 +4,54 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
-// import NewsIteams from './components/NewsIteams';
+// import News setProgress={this.setProgress} from './components/News setProgress={this.setProgress}';
+// import News setProgress={this.setProgress}Iteams from './components/News setProgress={this.setProgress}Iteams';
+import LoadingBar from 'react-top-loading-bar'
+
+// import LoadingBar from 'react-top-loading-bar'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   // Link
 } from "react-router-dom";
+// import News from './components/News';
 
 
 export default class App extends Component {
   pageSize=12;
+  state={
+    progress:20
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress});
+  }
   render() {
     return (
       <div>
          <Router>
-          {/* <News pageSize={6} country={"in"} category={"sports"}/> */}
+          {/* <News setProgress={this.setProgress} pageSize={6} country={"in"} category={"sports"}/> */}
         <Navbar/> 
-        {/* <News pageSize={6} country={"in"} category={"sports"}/> */}
+        <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        // onLoaderFinished={() => setProgress(0)}
+      />
         <Routes>
-           {/* <News pageSize={6} country={"in"} category={"sports"}/> */}
-        <Route exact path="/" element={<News pageSize={this.pageSize} key="general" country="in" category="general"/>} />
-        <Route exact path="/general" element={<News pageSize={this.pageSize} key="general" country="in" category="general"/>} />
-        <Route exact path="/sports" element={<News pageSize={this.pageSize} key="sports" country="in" category="sports"/>} />
-        <Route exact path="/science" element={<News pageSize={this.pageSize} key="science" country="in" category="science"/>} />
-        <Route exact path="/entertainment" element={<News pageSize={this.pageSize} key="entertainment" country="in" category="entertainment"/>} />
-        <Route exact path="/business" element={<News pageSize={this.pageSize} key="business" country="in" category="business"/>} />
-        <Route exact path="/technology" element={<News pageSize={this.pageSize} key="technology" country="in" category="technology"/>} />
-        <Route exact path="/health" element={<News pageSize={this.pageSize} key="health" country="in" category="health"/>} />
+           {/* <News setProgress={this.setProgress} pageSize={6} country={"in"} category={"sports"}/> */}
+        <Route exact path="/" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="general" country="in" category="general"/>} />
+        <Route exact path="/general" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="general" country="in" category="general"/>} />
+        <Route exact path="/sports" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="sports" country="in" category="sports"/>} />
+        <Route exact path="/science" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="science" country="in" category="science"/>} />
+        <Route exact path="/entertainment" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="entertainment" country="in" category="entertainment"/>} />
+        <Route exact path="/business" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="business" country="in" category="business"/>} />
+        <Route exact path="/technology" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="technology" country="in" category="technology"/>} />
+        <Route exact path="/health" element={<News setProgress={this.setProgress} pageSize={this.pageSize} key="health" country="in" category="health"/>} />
       </Routes>
 
       
          </Router>
-          {/* <News pageSize={6} country={"in"} category={"sports"}/> */}
+          {/* <News setProgress={this.setProgress} pageSize={6} country={"in"} category={"sports"}/> */}
       </div>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -18,35 +18,39 @@ export class News extends Component {
     document.title = this.props.category;
   }
   async updateNews() {
+    this.props.setProgress(20);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0b77d67d3b214c40a51a1fa422cdf55c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(40);
     let parsedData = await data.json();
+    this.props.setProgress(75);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
   }
-  handlerPrev = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  };
-  handlerNext = async () => {
+  // handlerPrev = async () => {
+  //   this.setState({ page: this.state.page - 1 });
+  //   this.updateNews();
+  // };
+  // handlerNext = async () => {
+  //   this.setState({ page: this.state.page + 1 });
+  //   this.updateNews();
+  // };
+  fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  };
-  fetchMoreData = async() => {
-    this.setState({page:this.state.page+1})
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0b77d67d3b214c40a51a1fa422cdf55c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
-      articles: this.state.articles.concat(this.parsedData.articles),
+      articles: this.state.articles.concat(parsedData.articles),
       totalResults: parsedData.totalResults,
       loading: false,
     });
@@ -54,39 +58,44 @@ export class News extends Component {
 
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h2 className="text-center" style={{ margin: "35px 0px" }}>
           NewsMonkey top {this.props.category} headlines
         </h2>
+        {/* console.log(this.state.articles.length); */}
 
         {/* {this.state.loding && <Spinner/>} */}
         <InfiniteScroll
           dataLength={this.state.articles.length}
+          // console.log(dataLength);
           next={this.fetchMoreData}
           hasMore={this.state.articles.length !== this.state.totalResults}
           loader={<Spinner />}
         >
-          <div className="row">
-            {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsIteams
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imgurl={element.urlToImage}
-                    newsurl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsIteams
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imgurl={element.urlToImage}
+                      newsurl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </InfiniteScroll>
 
-        {
-        /* <div className="container d-flex justify-content-between">
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -107,7 +116,7 @@ export class News extends Component {
             next&rarr;
           </button>
         </div> */}
-      </div>
+      </>
     );
   }
 }
